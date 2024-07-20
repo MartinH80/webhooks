3a5be934a9b2c04f5ab41915399d71dc80463009
Martin Herrman <132921173+MartinH80@users.noreply.github.com>
2024-07-20 22:13:49 +0200

    Fix syntax errors in webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -7,12 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] !== "GET" ) {
   echo "Only accepting GET requests, exiting.";
   exit(1);
 } else {
-  echo "Received GET request."
+  echo "Received GET request.";
 }
 
 if (isset($_GET['to']) && isset($_GET['subject']) && isset($_GET['message'])) {
   echo "Received the required arguments: to, subject and message.";
-  echo "Trying to send mail."
+  echo "Trying to send mail.";
   $success=mail(urldecode($_GET['to']), urldecode($_GET['subject'],urldecode($_GET['message'])));
   if ($success == false) {
     echo "Sending mail failed.";
@@ -28,4 +28,4 @@ if (isset($_GET['to']) && isset($_GET['subject']) && isset($_GET['message'])) {
 
 exit(0);
 
->
+?>
