ea11627d4d3be016cba2accb5d16aeb625030754
Martin Herrman <132921173+MartinH80@users.noreply.github.com>
2024-07-21 13:24:07 +0200

    Webhooks refactor

--- webhooks.php
+++ webhooks.php
@@ -1,31 +1,37 @@
 <?php
 
-echo $_SERVER['REQUEST_TIME'];
-echo $_SERVER['REMOTE_ADDR'];
-
 if ($_SERVER['REQUEST_METHOD'] !== "GET" ) {
-  echo "Only accepting GET requests, exiting.";
-  exit(1);
-} else {
-  echo "Received GET request.";
-}
-
-if (isset($_GET['to']) && isset($_GET['subject']) && isset($_GET['message'])) {
-  echo "Received the required arguments: to, subject and message.";
-  echo "Trying to send mail.";
-  $success=mail(urldecode($_GET['to']), urldecode($_GET['subject'],urldecode($_GET['message'])));
-  if ($success == false) {
-    echo "Sending mail failed.";
-    exit(1);
-  } else {
-    echo "Mail send succesfully.";
-  }
-} else {
-  echo "Missing to, subject and/or message parameters. Arguments must be URL Encoded. Example usage of this script: http://hostname/webhooks.php?to=receiver%40e-mail.com&subject=Test%20alert&message=Alert%20started";
-  echo "Exiting.";
+  echo "Only accepting GET requests.";
+  http_response_code(405);
   exit(1);
 }
 
-exit(0);
+if (!isset($_GET['to'])) {
+  echo "Missing mandatory argument 'to'.";
+  http_response_code(400);
+  exit(1);
+} else {
+  $to = $_GET['to'];
+}
+
+if (isset($_GET['subject'])) {
+  $subject = $_GET['subject'];
+} else {
+  $subject = "";
+}
+
+if (isset($_GET['message'])) {
+  $message = $_GET['message'];
+} else {
+  $message = "";
+}
+
+if (mail($to,$subject,$message)) {
+  http_response_code(200);
+  exit(0);
+} else {
+  http_response_code(500);
+  exit(1);
+}
 
 ?>
